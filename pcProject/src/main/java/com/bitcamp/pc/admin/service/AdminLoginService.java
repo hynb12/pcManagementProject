0b64af67a2bdca3172520adc4f9998f23eab7999
asd30 <asd30@LAPTOP-FET3N3BI>
2018-10-29 16:45:16 +0900

    2018.10.29 16:44 Admin Login (문경원)

---
+++ pcProject/src/main/java/com/bitcamp/pc/admin/service/AdminLoginService.java
@@ -0,0 +1,44 @@
+package com.bitcamp.pc.admin.service;
+
+import javax.servlet.http.HttpSession;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bitcamp.pc.member.dao.AdminDaoInterface;
+import com.bitcamp.pc.member.model.AdminVO;
+
+@Service
+public class AdminLoginService {
+
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	private AdminDaoInterface adminDao;
+	
+	
+	public boolean AdminLogin(String userId, String userPw, HttpSession session) {
+		
+		boolean result = false;
+	
+		adminDao = sqlSessionTemplate.getMapper(AdminDaoInterface.class);
+		AdminVO vo = adminDao.loginAdmin(userId);
+		
+		// 1. vo에는 id & pw를 받아오는데
+		// 1-1. vo의 값이 null이 아니라면 얻어오는 adminId 값과 검색한 adminId값이 일치해야한다. (adminPw도 동일)
+		if(vo != null && vo.getAdminId().equals(userId) && vo.getAdminPw().equals(userPw)) {
+			
+			// 2. session에 adminPw 값이 노출되지 않도록 Pw값은 초기화
+			vo.setAdminPw("");
+			
+			// 3. adminPw를 초기화하였다면 session에 저장
+			session.setAttribute("adminVO", vo);
+			
+			
+			// 모두 충족한다면 true 값으로 반환
+			result = true;
+		}
+		
+		return result;
+	}
+}
--- pcProject/src/main/java/com/bitcamp/pc/member/dao/AdminDaoInterface.java
+++ pcProject/src/main/java/com/bitcamp/pc/member/dao/AdminDaoInterface.java
@@ -3,5 +3,11 @@ package com.bitcamp.pc.member.dao;
 import com.bitcamp.pc.member.model.AdminVO;
 
 public interface AdminDaoInterface {
+	
+	// Admin 회원가입
 	public int insertAdmin(AdminVO admin);
+	
+	// Admin 로그인
+	public AdminVO loginAdmin(String userId);
+	
 }
